d9ab515348a2467586599c32446218df5073fa3d
marbsangr <47706205+marbsangr@users.noreply.github.com>
2019-02-23 01:14:44 -0600

    Add files via upload

---
+++ Practica 1.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Practica#1
+(ID_Alumno int PRIMARY KEY NOT NULL,
+Apellido_Paterno varchar (10) NOT NULL, 
+Apellido_Materno varchar (10) NOT NULL, 
+Nombres varchar (30) NULL, 
+Carrera varchar (50) NULL);
---
+++ Practica#1.sql
@@ -0,0 +1,3 @@
+create database Practica#1
+GO
+
